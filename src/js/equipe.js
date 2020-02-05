6c801023c59277f9a88e39856dd101bddd7009eb
David Binaud <david.binaud@etu.umontpellier.fr>
2020-02-05 13:06:03 +0100

    Ex1 1 => 4

--- src/js/equipe.js
+++ src/js/equipe.js
@@ -1,11 +1,19 @@
 class Equipe {
 
 	constructor(nom) {
-		// à compléter
+		this.nom = nom;
+		this.classement = 0;
+		this.points = 0;
+		this.matchWon = 0;
+		this.matchDraw = 0;
+		this.matchLost = 0;
+		this.goalsGiven = 0;
+		this.goalsReceived = 0;
+		this.evaluation = Math.random();
 	}
 
 	evaluer() {
-		// à compléter
+		return this.points * 10000 + (this.goalsGiven - this.goalsReceived) * 100 + this.goalsGiven + this.evaluation;
 	}
 
 	affichage() {
