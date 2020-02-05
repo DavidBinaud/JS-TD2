bc17cec100c6c86336186ef695500a2578c4fbf2
David Binaud <david.binaud@etu.umontpellier.fr>
2020-02-05 13:59:56 +0100

    Exo 2:  1 => 3

--- src/js/match.js
+++ src/js/match.js
@@ -1,7 +1,11 @@
 class Match {
 
 	constructor(eq1,eq2) {
-		// à compléter
+		this.eq1 = eq1;
+		this.eq2 = eq2;
+		this.res1 = 0;
+		this.res2 = 0;
+		this.played = false;
 	}
 
 	jouer() {
--- src/js/scriptSetup.js
+++ src/js/scriptSetup.js
@@ -1,7 +1,11 @@
 let eq1 = new Equipe("PSG");
-eq1.G = 6;
-eq1.N = 3;
-eq1.P = 2;
-eq1.bp = 23;
-eq1.bc = 8;
-eq1.mise_a_jour(2,1);
+// eq1.G = 6;
+// eq1.N = 3;
+// eq1.P = 2;
+// eq1.bp = 23;
+// eq1.bc = 8;
+// eq1.mise_a_jour(2,1);
+
+let eq2 = new Equipe("FCN");
+
+let match1 = new Match(eq1,eq2);
