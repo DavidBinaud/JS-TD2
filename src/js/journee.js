244111a576c6a0aba68a9d5cb77c38ebcc19549f
David Binaud <david.binaud@etu.umontpellier.fr>
2020-02-05 14:20:29 +0100

    Exo 3: 1 => 3

--- src/js/journee.js
+++ src/js/journee.js
@@ -1,7 +1,11 @@
 class Journee {
 
 		constructor(match1,match2,match3,match4) {
-			// à compléter
+			this.match1 = match1;
+			this.match2 = match2;
+			this.match3 = match3;
+			this.match4 = match4;
+			this.played = false;
 		}
 
 		afficher() {
--- src/js/scriptSetup.js
+++ src/js/scriptSetup.js
@@ -1,4 +1,5 @@
-let eq1 = new Equipe("PSG");
+// EXO 1 - 2
+// let eq1 = new Equipe("PSG");
 // eq1.G = 6;
 // eq1.N = 3;
 // eq1.P = 2;
@@ -6,14 +7,32 @@ let eq1 = new Equipe("PSG");
 // eq1.bc = 8;
 // eq1.mise_a_jour(2,1);
 
+// let eq2 = new Equipe("FCN");
+// eq1.affichage();
+// eq2.affichage();
+
+// let match1 = new Match(eq1,eq2);
+// match1.jouer();
+// match1.maj_equipes();
+// match1.affichage();
+
+// eq1.affichage();
+// eq2.affichage();
+
+
+// EXO 3
+let eq1 = new Equipe("PSG");
 let eq2 = new Equipe("FCN");
-eq1.affichage();
-eq2.affichage();
+let eq3 = new Equipe("OM");
+let eq4 = new Equipe("OL");
+let eq5 = new Equipe("ASE");
+let eq6 = new Equipe("MHSC");
+let eq7 = new Equipe("ASSE");
+let eq8 = new Equipe("EAG");
 
 let match1 = new Match(eq1,eq2);
-match1.jouer();
-match1.maj_equipes();
-match1.affichage();
+let match2 = new Match(eq3,eq4);
+let match3 = new Match(eq5,eq6);
+let match4 = new Match(eq7,eq8);
 
-eq1.affichage();
-eq2.affichage();
+let journee1 = new Journee(match1,match2,match3,match4);
