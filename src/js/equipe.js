283da1da701985cebcd75e41f0313d0029019624
David Binaud <david.binaud@etu.umontpellier.fr>
2020-02-05 13:43:21 +0100

    changements des noms

--- src/js/equipe.js
+++ src/js/equipe.js
@@ -3,21 +3,21 @@ class Equipe {
 	constructor(nom) {
 		this.nom = nom;
 		this.classement = 0;
-		this.points = 0;
-		this.matchWon = 0;
-		this.matchDraw = 0;
-		this.matchLost = 0;
-		this.goalsGiven = 0;
-		this.goalsReceived = 0;
+		this.pts = 0;
+		this.G = 0;
+		this.N = 0;
+		this.P = 0;
+		this.bp = 0;
+		this.bc = 0;
 		this.evaluation = Math.random();
 	}
 
 	evaluer() {
-		return this.points * 10000 + (this.goalsGiven - this.goalsReceived) * 100 + this.goalsGiven + this.evaluation;
+		return this.pts * 10000 + (this.bp - this.bc) * 100 + this.bp + this.evaluation;
 	}
 
 	affichage() {
-		console.log(this.classement.toString() + " " + this.nom + " " + this.points.toString() + " " + this.matchWon.toString() + " " + this.matchDraw.toString() + " " + this.matchLost.toString() + " " +  this.goalsGiven.toString() + " " + this.goalsReceived.toString() + " " + (this.goalsGiven - this.goalsReceived).toString());
+		console.log(this.classement.toString() + " " + this.nom + " " + this.pts + " " + this.G + " " + this.N + " " + this.P + " " +  this.bp + " " + this.bc + " " + (this.bp - this.bc));
 	} 
 
 	mise_a_jour(bp,bc) {
