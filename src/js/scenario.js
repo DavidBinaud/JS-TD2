f84db613ec77d333de95c4fcbf14afe34a3033fd
David Binaud <david.binaud@etu.umontpellier.fr>
2020-02-09 12:13:46 +0100

    Fin TD2

--- src/js/scenario.js
+++ src/js/scenario.js
@@ -19,42 +19,93 @@ let chp;
 // les styles d'affichages initiaux
 
 // fieldset J, C et E
-// à compléter
+fj.style.display = "none";
+fc.style.display = "none";
+fe.style.display = "inline";
 
 // div equipesEngagees et listeEquipes
-// à compléter
+ee.style.display = "inline";
+le.style.display = "none";
 
 // les images plus et moins
-// à compléter
+mo.style.display = "none";
 
 // les boutons journée suivante et jouer journée
-// à compléter
+jj.style.display = "inline";
+js.style.display = "none";
 
 // on remplit les div des équipes engagées
-// à compléter
+for (var i = 0; i < 8; i++) {
+	let input = document.getElementById("nomEqu" + (i+1));
+	input.value = tabEq[i];
+}
+
+ee.innerHTML = "équipes engagées : PSG - ASM - OL - OM - FCN - ASSE - MHSC - EAG";
 
 
 // programmation des gestions d'événements par des fonctions anonymes
 
 // clic sur l'image plus
 pl.onclick = function() {
-	// à compléter
+	pl.style.display = "none";
+   	mo.style.display = "inline";
+   	le.style.display = "inline";
+   	ee.style.display = "none";
 }
 
 // clic sur l'image moins
 mo.onclick = function() {
-	// à compléter
+	pl.style.display = "inline";
+   	mo.style.display = "none";
+   	le.style.display = "none";
+
+   	ee.innerHTML = "équipes engagées :";
+	for (var i = 0; i < 8; i++) {
+		let input = document.getElementById("nomEqu" + (i+1));
+		console.log(input.innerHTML);
+		ee.innerHTML += " " + input.value + " -";
+	}
+	ee.innerHTML = ee.innerHTML.slice(0,-2);
+   	ee.style.display = "inline";
 }
 
 // clic sur le bouton lancer championnat
 lc.onclick = function() {
-	// à compléter
+	let eq1 = new Equipe(document.getElementById("nomEqu1").value);
+	let eq2 = new Equipe(document.getElementById("nomEqu2").value);
+	let eq3 = new Equipe(document.getElementById("nomEqu3").value);
+	let eq4 = new Equipe(document.getElementById("nomEqu4").value);
+	let eq5 = new Equipe(document.getElementById("nomEqu5").value);
+	let eq6 = new Equipe(document.getElementById("nomEqu6").value);
+	let eq7 = new Equipe(document.getElementById("nomEqu7").value);
+	let eq8 = new Equipe(document.getElementById("nomEqu8").value);
+	chp = new Championnat(eq1,eq2,eq3,eq4,eq5,eq6,eq7,eq8);
+	chp.classer_equipes();
+	chp.afficher_classement();
+	document.getElementById("numJ").innerHTML = chp.numJournee;
+	chp.afficher_journee(chp.numJournee);
+	jj.style.display = "inline";
+	js.style.display = "none";
+	fj.style.display = "inline";
+	fc.style.display = "inline";
+	fe.style.display = "none";
+	lc.style.display = "none";
 }
 
 jj.onclick = function() {
-	// à compléter
+	chp.jouer_journee(chp.numJournee);
+	jj.style.display = "none";
+	if (chp.numJournee < 14) {
+		js.style.display = "inline";
+	}
+	chp.classer_equipes();
+	chp.afficher_classement();
 }
 
 js.onclick = function() {
-	// à compléter
+	chp.numJournee++;
+	chp.afficher_journee(chp.numJournee);
+	document.getElementById("numJ").innerHTML = chp.numJournee;
+	jj.style.display = "inline";
+	js.style.display = "none";
 }
